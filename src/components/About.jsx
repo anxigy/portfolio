be63e58cb7d934b1e1dcd6a3d3b7a05559240d83
lucy <lucy@lucyui-MacBookPro.local>
2025-03-18 16:30:23 +0900

    메인 페이지 디자인 변경

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,111 +19,162 @@ const Container = styled.section`
   text-align: center;
   font-size: 50pt;
   height: 100%;
+
+  position: relative;
 `
 
 const MainWrap = styled.div`
-  width: 70%;
+  width: 75%;
   height: 100%;
   // background-color: #000;
 `
 
 const MainSection = styled.div`
   width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
+  /* display: flex; */
+  /* justify-content: space-between;
+  align-items: flex-end; */
   height: 80%;
-
 
   @media (max-width: 1100px) {
     flex-direction: column;
-    align-items: flex-start;
     justify-content: center;
+    align-items: center;
     height: 100%;
-    padding-top: 3rem;
+    padding-top: 2rem;
+    display: flex;
   }
 `
-const TextSection = styled.div`
-  width: 70%;
 
-
-  @media (max-width: 639px) {
-    width: 100%;
-  }
-`
 const ProfileSection = styled.div`
-  width: 30%;
-  height: 70%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`
+const ProfileWrapper = styled.div`
+  display: flex;
+  z-index: 20;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #D9D9D9;
+  border-radius: 50%;
+  width: 370px;
+  height: 370px;
+  box-shadow: rgba(255, 255, 255, 0.5) 0px 7px 29px 0px;
+
   @media (max-width: 639px) {
-    height: 200px;
-    width: auto;
+    width: 300px;
+    height: 300px;
     margin-top: 20px;
     text-align: center;
   }
-   @media (max-width: 1100px) and (min-width: 640px) {
-    width: auto;
-    height: 500px;
+   /* @media (max-width: 1100px) and (min-width: 640px) {
+    width: 300px;
+    height: 300px;
     margin-top: 2rem;
-  }
+  } */
   
 `
 
+
 const MainTitle = styled.h1`
-  color: #fff;
-  font-size: 100px;
-  white-space: pre-wrap;
-  text-align: left;
-  font-family: "Archivo Black", serif;
-  font-weight: 400;
+  width: 100%;
+  color: white;
+  font-size: 110px;
+  font-family: "Dela Gothic One", sans-serif;
   font-style: normal;
-
-
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
   @media (max-width: 639px) {
-    font-size: 60px;
-    white-space: pre;
+    font-size: 30px;
+    white-space: pre-wrap;
+    display: block;
   }
-`
+
+   @media (max-width: 900px) and (min-width: 600px) {
+    font-size: 80px;
+  }
+`;
+
+const OutlineText = styled.h1`
+  font-size: 70px;
+  font-weight: bold;
+  color: transparent;
+  -webkit-text-stroke: 0.1px #cbcbcb; 
+  font-family: "Archivo Black", serif;
+  text-align: left;
+  @media (max-width: 639px) {
+    font-size: 30px;
+    white-space: pre-wrap;
+  }
+`;
 
 const Description = styled.p`
   color: #fff;
   font-size: 16px;
-  white-space: pre-wrap;
   text-align: left;
   font-family: "Pretendard Variabl";
   font-weight: 400;
   font-style: normal;
   line-height: 25px;
-  margin-top: 3rem;
 `
-
-const BoldText = styled.span`
-  font-weight: bold;
+const ProfileImage = styled.img`
+  width: 220px;
+  height: 290px;
+  @media (max-width: 639px) {
+    width: 180px;
+    height: 250px;
+  }
 `;
 
-export const About = ({screenRef}) => {
+
+const DiagonalLine = styled.div`
+  position: absolute;
+  width: 120%;
+  height: 100px;
+  background: rgba(255, 255, 255, 0.3);
+  transform: rotate(-23deg);
+  top: 50%;
+  overflow: hidden;
+  z-index: 10;
+`;
+
+const FooterWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  align-items: flex-end;
+  @media (max-width: 639px) {
+    flex-direction: column;
+    margin-top: 20px;
+  }
+`
+
+export const About = ({ screenRef }) => {
 
   return (
     <Container id="Container" >
-      {/* <MainTitle>{title}</MainTitle> */}
-      <MainWrap >
+      <DiagonalLine />
+      <MainWrap>
         <MainSection ref={screenRef}>
-          <TextSection>
-            <MainTitle>{`Hello\nI'm Lucy`}</MainTitle>
-            <Description>
-              {`안녕하세요. `}
-              <BoldText>4년차</BoldText>
-              {` 프론트엔드 개발자 `}
-              <BoldText>남가영</BoldText>
-              {`입니다.\n누구보다 끈기가 강하고 새로운 것을 배우는 것에 대하여 거리낌 없다고 당당하게 이야기 할 수 있습니다.`}
-            </Description>
-          </TextSection>
+          <MainTitle>{`Hello I'm Lucy`}</MainTitle>
+          <OutlineText>FRONTEND</OutlineText>
           <ProfileSection>
-            {
-              isMobile ?  
-              <img src={require('../assets/avatar2.png')} alt="" width={150} height={200}/> : 
-              <img src={require('../assets/avatar2.png')} alt="" width={'100%'} height={'100%'}/>
-            }
+            <ProfileWrapper>
+              {
+                isMobile ?
+                  <ProfileImage src={require('../assets/avatar2.png')} alt="" /> :
+                  <ProfileImage src={require('../assets/avatar2.png')} alt="" />
+              }
+            </ProfileWrapper>
           </ProfileSection>
+          <FooterWrapper>
+            <Description>E-MAIL. nsh5438@naver.com</Description>
+            <OutlineText style={{ textAlign: 'right' }}>DEVELOPER</OutlineText>
+          </FooterWrapper>
         </MainSection>
       </MainWrap>
     </Container>
